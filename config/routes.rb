95345a221c93afd63abdfef84a44920b6dcb351c
Gleidson Silva <gleidson.fgs@gmail.com>
2018-01-06 02:22:05 -0300

    alterando configuracao do background

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :contacts, only: [:new, :create]
+  # resources :contacts, only: [:new, :create]
+
+  get "contacts/new"
+
   resources :tours, only: [:new, :create, :edit, :update, :destroy]
   resources :videos, only: [:new, :create, :edit, :update, :destroy]
   root "home#index"
