f2770c159367da694cd57236979138dd57427704
Gleidson Silva <gleidson.fgs@gmail.com>
2018-01-29 11:18:20 -0300

    producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
     :password => ENV["GMAIL_PASSWORD"]
   }
 
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
